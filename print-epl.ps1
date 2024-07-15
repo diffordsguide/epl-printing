ef25cd5d064a2cb2247968ce4b662d6755d6745e
Harry Webster <harrywebster@users.noreply.github.com>
2024-07-15 11:12:24 +0100

    Update print-epl.ps1

--- print-epl.ps1
+++ print-epl.ps1
@@ -4,7 +4,10 @@ $eplFile=$args[0]
 
 write-host "Printing $eplFile, please wait..."
 
-& cmd.exe /c COPY /B $eplFile \\127.0.0.1\DPD 2`>`&1
+cmd.exe /c COPY /B $eplFile \\127.0.0.1\DPD
+
+# removed & and handler redirects, these no longer work in Windows 11
+#& cmd.exe /c COPY /B $eplFile \\127.0.0.1\DPD 2`>`&1
 
 Write-Host "Deleting temp file $eplFile"
 rm $eplFile
